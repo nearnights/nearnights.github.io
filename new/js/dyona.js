7ed02cc6802be705fdfb22a7766c6cde737cac19
nearnights <neardyona@gmail.com>
2018-11-22 18:28:01 +0900

    change css / js / html

--- new/js/dyona.js
+++ new/js/dyona.js
@@ -16,14 +16,45 @@ $(document).ready(function(){
 			height:set + 'px'
 		})
 	}
+
+	var wh = $(window).height();
+	var header = $('.header').height();
+	var ch = $('.container').height();
+	var oneh = wh - header;
+	$('.section').css({
+		height:ch + 'px'
+	})
+	$(window).on('resize', function(){
+		var wh = $(window).height();
+		var header = $('.header').height();
+		var ch = $('.container').height();
+		var oneh = wh - header;
+		$('.section').css({
+			height:ch + 'px'
+		})
+	})
+
 	$('.container').slimScroll({
         size : '7px',
         color: '#333',
 		opacity : 1,
         railColor : '#333',
         railOpacity : .2,
-        wheelStep : 8,
+        wheelStep : 20,
 		touchScrollStep : 100,
         railBorderRadius : '7px'
 	});
+
+	scrollOpacity();
+
+	function scrollOpacity(){
+		$('.container').scroll(function () {
+			var scroll = $('.container').scrollTop()
+			if (scroll > 0) {
+				$('.btn').stop().animate({opacity:0},100)
+			} else{
+				$('.btn').stop().animate({opacity:1},100)
+			}
+		}
+	)}
 })
